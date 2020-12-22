3ce6f3913c51e6ce0f53af7234236567e978ac19
Saurabh jain <s2k1jain@gmail.com>
2020-12-22 16:25:11 +0530

    initial author routes

---
+++ routes/authors.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const author = require('../Models/author')
+const router = express.Router()
+const Author = require('../Models/author')
+// all authors route
+router.get('/',async (req, res) => {
+    let searchOptions={}
+    if(req.query.name!=null && req.query.name!==''){
+        searchOptions.name=new RegExp(req.query.name,'i')
+    }
+    try{
+        const authors= await Author.find(searchOptions)
+        res.render('authors/index',{
+            authors:authors,
+            searchOptions:req.query
+        })
+    } catch{
+         res.redirect('/')
+    }
+    
+})
+// new author route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() })
+})
+// create author route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    
+    try {
+        const newAuthor = await author.save()
+        res.redirect('authors')
+    } catch {
+        // let locals = { errorMessage : 'something went wrong' }
+        res.render('authors/new', {
+            author:author,
+            locals:{ errorMessage : 'something went wrong' }
+        })
+    }
+
+})
+module.exports = router
